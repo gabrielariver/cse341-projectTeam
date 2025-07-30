c459e39423310d9364f1e2322189a5a9a50739bd
gabrielariver <riv23011@byui.edu>
2025-07-30 17:27:43 -0500

    Fix: correct syntax error in users.js export

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,9 @@ const router = express.Router();
 const userController = require('../controllers/users');
 const { ensureAuth } = require('../middleware/ensureAuth');
 
-// Public routes
+
 router.get('/', userController.getAllUsers);
 router.get('/:username', userController.getUserByUsername);
-
-// Protected routes
 router.post('/', ensureAuth, userController.createUser);
 router.put('/:username', ensureAuth, userController.updateUser);
 router.delete('/:username', ensureAuth, userController.deleteUser);
