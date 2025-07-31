4bfd8606c6f1d2f92fbfcb95af24fc5e8b0b3e02
gabrielariver <riv23011@byui.edu>
2025-07-31 00:16:01 -0500

    Fix PUT endpoint

--- models/recipe.js
+++ models/recipe.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({
   userId: {
     type: String,
-    required: true,
+    required: false,
   },
   title: {
     type: String,
