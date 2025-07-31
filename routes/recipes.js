7719f1d4fdfbf6bad13bf38ffd5ee6d310839145
gabrielariver <riv23011@byui.edu>
2025-07-30 19:52:16 -0500

    ouath for the next week

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const recipeController = require('../controllers/recipes');
-const ensureAuth = require('../middleware/ensureAuth');
+//const ensureAuth = require('../middleware/ensureAuth');
 
 router.get('/', recipeController.getAllRecipes);
 router.get('/:id', recipeController.getRecipeById);
-router.post('/', ensureAuth, recipeController.createRecipe);
-router.put('/:id', ensureAuth, recipeController.updateRecipe);
-router.delete('/:id', ensureAuth, recipeController.deleteRecipe);
+router.post('/', recipeController.createRecipe);
+router.put('/:id', recipeController.updateRecipe);
+router.delete('/:id', recipeController.deleteRecipe);
+
+//router.get('/', recipeController.getAllRecipes);
+//router.get('/:id', recipeController.getRecipeById);
+//router.post('/', ensureAuth, recipeController.createRecipe);
+//router.put('/:id', ensureAuth, recipeController.updateRecipe);
+//router.delete('/:id', ensureAuth, recipeController.deleteRecipe);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users');
-const ensureAuth = require('../middleware/ensureAuth');
+//const ensureAuth = require('../middleware/ensureAuth');
 
 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById );
-router.post('/', ensureAuth, userController.createUser);
-router.put('/:id', ensureAuth, userController.updateUser);
-router.delete('/:id', ensureAuth, userController.deleteUser);
+router.post('/', userController.createUser);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
+
+//router.get('/', userController.getAllUsers);
+//router.get('/:id', userController.getUserById );
+//router.post('/', ensureAuth, userController.createUser);
+//router.put('/:id', ensureAuth, userController.updateUser);
+//router.delete('/:id', ensureAuth, userController.deleteUser);
 
 module.exports = router;
