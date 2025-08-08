56f15c1bb2fb85f2b52f55f9c111d1a1c49ae926
gabrielariver <riv23011@byui.edu>
2025-08-08 01:10:40 -0500

    refactor: update favorite schema to include userId and adjust Swagger documentation accordingly

--- models/favorite.js
+++ models/favorite.js
@@ -1,22 +1,19 @@
 const mongoose = require('mongoose');
 
-const favoriteSchema = new mongoose.Schema(
-  {
-    recipeId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Recipe',
-      required: true
-    },
-    userId: {
-      type: String,
-      required: true
-    },
-    addedAt: {
-      type: Date,
-      default: Date.now
-    }
+const favoriteSchema = new mongoose.Schema({
+  recipeId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Recipe',
+    required: true
   },
-  { collection: 'favorite' } 
-);
+  userId: {
+    type: String,
+    required: true
+  },
+  addedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 module.exports = mongoose.model('Favorite', favoriteSchema);
